dc37abd223590987c8a0d72df08e5569c4184281
Santos-OK <0262142@up.edu.mx>
2025-09-24 20:37:49 -0600

    Delete galleriapp/src/components/title.jsx

--- galleriapp/src/components/title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
- function Title() {
-  return (
-    <div className='title'><h1>La magia no es un cuento, yo he visto magos reales</h1></div>
-  )
-}
-
-export default Title
